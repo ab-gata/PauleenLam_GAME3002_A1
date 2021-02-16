1637f90a234a00f8e966c3be873facc0e599e053
Pauleen Lam <pauleen.lam@outlook.com>
2021-02-16 02:24:13 -0500

    score

--- Assets/Scripts/HoopBehaviour.cs
+++ Assets/Scripts/HoopBehaviour.cs
@@ -3,23 +3,24 @@ using UnityEngine;
 
 public class HoopBehaviour : MonoBehaviour
 {
-    //private Rigidbody m_rHoop = null;
+    [SerializeField]
+    private GameObject m_rHoop = null;
 
-    //protected float fLevel = 0;
+    protected float fLevel = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //m_rHoop = GetComponent<Rigidbody>();
-        //Assert.IsNotNull(m_rHoop, "ERROR: No rigid body on hoop...");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (transform.position.x > 10)
-        //    transform.position.x = new Vector3(-1, 0, 0);
+
+        //if (m_rHoop.transform.position.x > 10)
+        //    m_rHoop.transform.position.x++;
         //if (transform.position.x < -10)
         //    m_rHoop.velocity = new Vector3(1, 0, 0);
     }
